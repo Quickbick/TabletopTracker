03cd79fb5b665155e7e8e56cecd877df15504780
DHardie1201 <143314348+DHardie1201@users.noreply.github.com>
2023-10-02 03:05:23 -0700

    Delete fxproject/src/main/java/com/cs422/fxproject/creatures/Conditions.java

--- fxproject/src/main/java/com/cs422/fxproject/creatures/Conditions.java
+++
@@ -1,7 +0,0 @@
-package com.cs422.fxproject.creatures;
-
-public enum Conditions {
-    Blinded, Charmed, Deafened, Frightened, Grappled,
-    Incapacitated, Invisible, Paralyzed, Petrified,
-    Poisioned, Prone, Restrained, Stunned, Unconcious
-}
